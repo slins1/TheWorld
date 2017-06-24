f284392ccf6266812a1494f3f52547308f658bad
Spencer Linsner <sjlinsner@gmail.com>
2017-06-24 15:21:44 -0400

    More mail service modifications

--- TheWorld/Controllers/Web/AppController.cs
+++ TheWorld/Controllers/Web/AppController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +13,13 @@ namespace TheWorld.Controllers.Web
     public class AppController : Controller
     {
         private IMailService _mailService;
+        private IConfigurationRoot _config;
 
-        public AppController(IMailService mailService)
+        public AppController(IMailService mailService, IConfigurationRoot config)
         {
             _mailService = mailService;
+            _config = config;
+
         }
 
         public IActionResult Index()
@@ -27,11 +31,19 @@ namespace TheWorld.Controllers.Web
         {
             return View();
         }
-
+        
         [HttpPost]
         public IActionResult Contact(ContactViewModel model)
         {
+            if (model.Email.Contains("aol.com")) ModelState.AddModelError("Email", "We don't support AOL addresses");
 
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "From TheWorld", model.Message);
+
+                ModelState.Clear();
+                ViewBag.UserMessage = "Message Sent";
+            }
 
             return View();
         }
