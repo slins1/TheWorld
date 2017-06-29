1410ac5ab12a16d30c71c66c05d59b9f5dcdbd31
Spencer Linsner <sjlinsner@gmail.com>
2017-06-28 23:50:02 -0400

    Repository pattern stuff

---
+++ TheWorld/Models/WorldRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace TheWorld.Models
+{
+    public class WorldRepository : IWorldRepository
+    {
+        private WorldContext _context;
+        private ILogger<WorldRepository> _logger;
+
+        public WorldRepository(WorldContext context, ILogger<WorldRepository> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public IEnumerable<Trip> GetAllTrips()
+        {
+            _logger.LogInformation("Getting All Trips form the Database");
+
+            return _context.Trips.ToList();
+        }
+    }
+}
