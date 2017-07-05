c0251b8e3064a824738c41b04fde547236b4caa0
Spencer Linsner <sjlinsner@gmail.com>
2017-07-04 21:16:24 -0400

    Add API functionality

---
+++ TheWorld/Api/TripsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TheWorld.Models;
+
+namespace TheWorld.Api
+{
+    public class TripsController : Controller
+    {
+        private IWorldRepository _repository;
+
+        public TripsController(IWorldRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("api/trips")]
+        public IActionResult Get()
+        {
+            return Ok(_repository.GetAllTrips());
+        }
+    }
+}
